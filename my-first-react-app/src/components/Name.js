ef6401d38e7bf7c0ce2f271cab76c572e723a49f
Dmitriy Velikiy <velikiy.dmitriy91@gmail.com>
2021-01-19 00:01:03 +0200

    MRT хеадер на реакте

--- my-first-react-app/src/components/Name.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Name() {
-    return (
-        <h2>Velikiy Dmitriy</h2>
-    )
-}
--- my-first-react-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//Parts
-
-import { App } from './container/App';
-
-ReactDOM.render(<App />, document.getElementById('name'));
-
